ba65f2058b28b6f826d5d00e61c8749a627aeb56
gabrieladia1979 <gadia@uade.edu.ar>
2025-07-10 01:07:03 -0300

    change principal page to spa

--- estudioContable/src/pages/HomePage.jsx
+++ estudioContable/src/pages/HomePage.jsx
@@ -1,11 +1,15 @@
 // src/pages/HomePage.jsx
 import React from 'react';
+import Navbar from '../components/layout/Navbar';
+import Footer from '../components/layout/Footer';
 import Hero from '../components/sections/Hero';
 
 const HomePage = () => {
   return (
     <div>
+      <Navbar />
       <Hero />
+      <Footer />
     </div>
   );
 };
